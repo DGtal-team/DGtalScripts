c4fcdb9ff7beb8829e11389f426c467f8f706af8
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2011-09-08 12:48:06 +0200

    Dev scripts added

--- dashboard/DGtal_debug.vbox.vs9.cmake
+++ dashboard/DGtal_debug.vbox.vs9.cmake
@@ -19,6 +19,25 @@ set(dashboard_git_branch master)
 set(dashboard_root_name   "Dashboard/MyTests/DGtal/${CTEST_BUILD_NAME}")
 
 SET(dashboard_cache "
+//Path to a file.
+Boost_INCLUDE_DIR:PATH=C:/Program Files/boost/boost_1_47
+
+//Boost library directory
+Boost_LIBRARY_DIRS:FILEPATH=C:/Program Files/boost/boost_1_47/lib
+
+//Boost diagnostic define
+Boost_LIB_DIAGNOSTIC_DEFINITIONS:STRING=-DBOOST_LIB_DIAGNOSTIC
+
+//The Boost PROGRAM_OPTIONS library
+Boost_PROGRAM_OPTIONS_LIBRARY:FILEPATH=optimized;C:/Program Files/boost/boost_1_47/lib/libboost_program_options-vc90-mt-1_47.lib;debug;C:/Program Files/boost/boost_1_47/lib/libboost_program_options-vc90-mt-gd-1_47.lib
+
+//Path to a library.
+Boost_PROGRAM_OPTIONS_LIBRARY_DEBUG:FILEPATH=C:/Program Files/boost/boost_1_47/lib/libboost_program_options-vc90-mt-gd-1_47.lib
+
+//Path to a library.
+Boost_PROGRAM_OPTIONS_LIBRARY_RELEASE:FILEPATH=C:/Program Files/boost/boost_1_47/lib/libboost_program_options-vc90-mt-1_47.lib
+
+BUILD_TESTING:BOOL=ON
 WITH_ALL:BOOL=OFF
 //With CairoGraphics.
 WITH_CAIRO:BOOL=OFF
