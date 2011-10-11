0e7681e393acb64c478e1f4e475826cdc7ed02fe
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2011-10-11 17:39:47 +0200

    @writers instead of @author in doc

--- dashboard/DGtal_debug.vbox.nmake.cmake
+++ dashboard/DGtal_debug.vbox.nmake.cmake
@@ -5,6 +5,7 @@ SET (CTEST_ENVIRONMENT
    "PATH=C:/Program Files/Microsoft Visual Studio 9.0/Common7/IDE\;C:/Program Files/Microsoft Visual Studio 9.0/VC/BIN\;C:/Program Files/Microsoft Visual Studio 9.0/Common7/Tools\;C:/WINDOWS/Microsoft.NET/Framework/v3.5\;C:/WINDOWS/Microsoft.NET/Framework/v2.0.50727\;C:/Program Files/Microsoft Visual Studio 9.0/VC/VCPackages\;C:/Program Files/Microsoft SDKs/Windows/v6.0A/bin\;C:/WINDOWS/system32\;C:/WINDOWS;C:/WINDOWS/System32/Wbem;")
 
 
+
 set(CTEST_SITE "liris.vbox.nmake")
 set(CTEST_CMAKE_GENERATOR "NMake Makefiles")
 set(CTEST_BUILD_NAME "DGtal-debug.liris.vbox.nmake")
