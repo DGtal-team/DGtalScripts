d5e8dc17f410de778f30a5b493cd814256780453
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2011-11-18 12:46:38 +0100

    clang++ test suite added"

---
+++ dashboard/DGtal_debug.liris.ubuntu.clang++.64.cmake
@@ -0,0 +1,40 @@
+#  dcoeurjo
+set(CTEST_SITE "liris.ubuntu.gcc.64")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_NAME "DGtal-debug.ubuntu.clang++.64")
+set(CTEST_BUILD_CONFIGURATION Debug)
+SET(CTEST_BUILD_FLAGS -j10)
+
+
+
+
+find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
+find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+
+
+SET(dashboard_do_coverage true)
+SET(dashboard_do_memcheck true)
+set(dashboard_git_url "git://github.com/DGtal-team/DGtal.git")
+set(dashboard_git_branch master)
+set(dashboard_root_name   "dashboard/MyTests/DGtal/${CTEST_BUILD_NAME}")
+
+SET(dashboard_cache "
+CMAKE_CXX_COMPILER:STRING=/usr/bin/clang++                                                                                                     WITH_ALL:BOOL=OFF
+//With CairoGraphics.
+WITH_CAIRO:BOOL=OFF
+//With COIN3D & SOQT for 3D visualization (Qt required).
+WITH_COIN3D-SOQT:BOOL=OFF
+//With Gnu Multiprecision Library (GMP).
+WITH_GMP:BOOL=OFF
+//With Insight Toolkit ITK.
+WITH_ITK:BOOL=OFF
+//With GraphicsMagick++.
+WITH_MAGICK:BOOL=OFF
+//With LibQGLViewer for 3D visualization (Qt required).
+WITH_QGLVIEWER:BOOL=OFF"
+)
+
+include(${CTEST_SCRIPT_DIRECTORY}/cmake_common.cmake)
+
+
+
--- dashboard/testAll-Unixes.liris.sh
+++ dashboard/testAll-Unixes.liris.sh
@@ -8,3 +8,8 @@ ctest -j 10 -S DGtal_debug_all.liris.ubuntu.gcc.64.cmake
 ctest -j 10 -S DGtal_release_all.liris.ubuntu.gcc.64.cmake 
 ctest -j 10 -S DGtal_debug.liris.ubuntu.gcc.64.cmake      
 ctest -j 10 -S DGtal_release.liris.ubuntu.gcc.64.cmake 
+
+ctest -j 10 -S DGtal_debug_all.liris.ubuntu.clang++.64.cmake  
+ctest -j 10 -S DGtal_release_all.liris.ubuntu.clang++.64.cmake 
+ctest -j 10 -S DGtal_debug.liris.ubuntu.clang++.64.cmake      
+ctest -j 10 -S DGtal_release.liris.ubuntu.clang++.64.cmake 
