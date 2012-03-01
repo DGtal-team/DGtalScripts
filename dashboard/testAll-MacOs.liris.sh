b200a7f8d92d9c12112717eb33634f550d884ee0
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2012-03-01 13:28:58 +0100

    SCripting macosX

--- dashboard/testAll-MacOs.liris.sh
+++ dashboard/testAll-MacOs.liris.sh
@@ -4,7 +4,7 @@
 ## Ctest MacOs.liris.gcc
 cd ~/Sources/DGtalScripts/dashboard/ 
 
-ctest -j 10 -S DGtal_debug_all.liris.macosX.defaultGcc.cmake  
-ctest -j 10 -S DGtal_release_all.liris.macosX.defaultGcc.cmake 
-ctest -j 10 -S DGtal_debug.liris.macosX.defaultGcc.64.cmake      
-ctest -j 10 -S DGtal_release.liris.macosX.defaultGcc.cmake 
+ctest -S DGtal_debug_all.liris.macosX.defaultGcc.cmake  
+ctest -S DGtal_release_all.liris.macosX.defaultGcc.cmake 
+ctest -S DGtal_debug.liris.macosX.defaultGcc.64.cmake      
+ctest -S DGtal_release.liris.macosX.defaultGcc.cmake 
