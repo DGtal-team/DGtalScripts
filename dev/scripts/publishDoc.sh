c205f754ff6ea957454a94cde5772560f48b9b7f
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2012-12-30 10:00:12 +0100

    Update with new doc location
    Supposed to be in {DGtal}/build/html

--- dev/scripts/publishDoc.sh
+++ dev/scripts/publishDoc.sh
@@ -5,6 +5,6 @@ if ! test -d "${DGtal}"; then
     exit 1
 fi
 
-rsync -azv --delete --delete-after ${DGtal}/doc/html/ dgtal@liris.cnrs.fr:/home/dgtal/public_html/doc/$1/
+rsync -azv --delete --delete-after ${DGtal}/build/html/ dgtal@liris.cnrs.fr:/home/dgtal/public_html/doc/$1/
 
 echo "--> done."
