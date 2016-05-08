06a82183a6f5b850952ec2da2e0e7d17ab612212
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-05-08 17:24:46 +0200

    doc script

--- dev/scripts/publishDoc.sh
+++ dev/scripts/publishDoc.sh
@@ -6,5 +6,7 @@ if ! test -d "${DGtal}"; then
 fi
 
 rsync -azv --delete --delete-after ${DGtal}/build/html/ dgtal@liris.cnrs.fr:/home/dgtal/public_html/doc/$1/
+scp  ${DGtal}/build/DGtal-tagfile dgtal@liris.cnrs.fr:/home/dgtal/public_html/doc/nightly/tags/
+scp  ${DGtal}/build/Board-tagfile dgtal@liris.cnrs.fr:/home/dgtal/public_html/doc/nightly/tags/
 
 echo "--> done."
