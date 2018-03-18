019b5403b868e568af633ab03df0ba1c534f23f1
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-03-18 09:56:52 +0100

    new liris address

--- dev/scripts/publishDoc.sh
+++ dev/scripts/publishDoc.sh
@@ -5,8 +5,8 @@ if ! test -d "${DGtal}"; then
     exit 1
 fi
 
-rsync -azv --delete --delete-after ${DGtal}/build/html/ dgtal@connect.liris.cnrs.fr:/home/dgtal/public_html/doc/$1/
-scp  ${DGtal}/build/DGtal-tagfile dgtal@connect.liris.cnrs.fr:/home/dgtal/public_html/doc/nightly/tags
-scp  ${DGtal}/build/Board-tagfile dgtal@connect.liris.cnrs.fr:/home/dgtal/public_html/doc/nightly/tags
+rsync -azv --delete --delete-after ${DGtal}/build/html/ dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/$1/
+scp  ${DGtal}/build/DGtal-tagfile dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/nightly/tags
+scp  ${DGtal}/build/Board-tagfile dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/nightly/tags
 
 echo "--> done."
